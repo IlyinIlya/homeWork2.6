48a2faf72de1d5f3389b6efac6c64613993d8dbb
IlyinIO <ilyin88ok@gmail.com>
2024-05-20 22:13:10 +0700

    1. First step

---
+++ src/main/java/com/ArtCompany/ArtCompany/ArtCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.ArtCompany.ArtCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtCompanyApplication.class, args);
+    }
+
+}
