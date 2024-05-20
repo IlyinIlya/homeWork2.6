27b7190cc8a1dec8be019b8bd796f883e1bb3bb7
IlyinIO <ilyin88ok@gmail.com>
2024-05-21 02:27:25 +0700

    1. First step 2. Last step 3. Correction

--- src/main/java/Exceptions/EmployeeAlreadyAddedException.java
+++ src/main/java/Exceptions/EmployeeAlreadyAddedException.java
@@ -3,7 +3,7 @@ package Exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_GATEWAY)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EmployeeAlreadyAddedException extends RuntimeException {
     public EmployeeAlreadyAddedException(String message) {
         super(message);
--- src/main/java/com/ArtCompany/ArtCompany/EmployeeController.java
+++ src/main/java/com/ArtCompany/ArtCompany/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.ArtCompany.ArtCompany;
 
+import Exceptions.EmployeeAlreadyAddedException;
+import Exceptions.EmployeeNotFoundException;
+import Exceptions.EmployeeStorageIsFullException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,19 +26,37 @@ public class EmployeeController {
     @RequestMapping(path = "/add")
     public Employee addPersonal(@RequestParam("lastName") String lastName,
                                 @RequestParam("firstName") String firstName) {
-        return employeeService.addPersonal(lastName, firstName);
+        try {
+            return employeeService.addPersonal(lastName, firstName);
+        } catch (EmployeeStorageIsFullException e) {
+            System.out.println("Достигнут предел сотрудников");
+            throw e;
+        } catch (EmployeeAlreadyAddedException e) {
+            System.out.println("Сотрудник уже существует");
+            throw e;
+        }
     }
 
     @RequestMapping(path = "/remove")
     public Employee delPersonal(@RequestParam("lastName") String lastName,
                                 @RequestParam("firstName") String firstName) {
-        return employeeService.delPersonal(lastName, firstName);
+        try {
+            return employeeService.delPersonal(lastName, firstName);
+        } catch (EmployeeNotFoundException e) {
+            System.out.println("Сотрудник не найден");
+            throw e;
+        }
     }
 
     @RequestMapping(path = "/find")
     public Employee findPersonal(@RequestParam("lastName") String lastName,
                                  @RequestParam("firstName") String firstName) {
-        return employeeService.findPersonal(lastName, firstName);
+        try {
+            return employeeService.findPersonal(lastName, firstName);
+        } catch (EmployeeNotFoundException e) {
+            System.out.println("Сотрудник не найден");
+            throw e;
+        }
     }
 
     @GetMapping
